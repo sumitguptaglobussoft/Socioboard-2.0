0459337e0fac53d48f9cb65556ef965ef54d3d12
sumitglobussoft <sumit@globussoft.com>
2014-07-30 11:46:53 +0530

    First Full Code Commit Date [7/30/14]

---
+++ Socioboard/Socioboard/App_Start/CustomViewLocationRazorViewEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+
+
+
+namespace Socioboard.App_Start
+{
+
+    /// <summary>
+    /// This class 
+    /// </summary>
+    public class CustomViewLocationRazorViewEngine : RazorViewEngine
+    {
+        public CustomViewLocationRazorViewEngine()
+        {
+            ViewLocationFormats = new[] 
+        {
+            "~/Themes/Socioboard/Views/{1}/{0}.cshtml", "~/Themes/Socioboard/Views/{1}/{0}.vbhtml",
+            "~/Themes/Socioboard/Views/Shared/{0}.cshtml", "~/Themes/Socioboard/Views/Shared/{0}.vbhtml"
+        };
+
+            MasterLocationFormats = new[] 
+        {
+            "~/Themes/Socioboard/Views/{1}/{0}.cshtml", "~/Themes/Socioboard/Views/{1}/{0}.vbhtml",
+            "~/Themes/Socioboard/Views/Shared/{0}.cshtml", "~/Themes/Socioboard/Views/Shared/{0}.vbhtml"
+        };
+
+            PartialViewLocationFormats = new[] 
+        {
+            "~/Themes/Socioboard/Views/{1}/{0}.cshtml", "~/Themes/Socioboard/Views/{1}/{0}.vbhtml",
+            "~/Themes/Socioboard/Views/Shared/{0}.cshtml", "~/Themes/Socioboard/Views/Shared/{0}.vbhtml"
+        };
+        }
+    }
+
+
+}
